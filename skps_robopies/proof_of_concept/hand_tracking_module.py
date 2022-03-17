0901a811dd2961eb8e0118ce3ac3e555eb6d3915
Patryk Bandyra <patryk.bandyra@gmail.com>
2022-03-17 19:25:44 +0100

    Initial commit

---
+++ skps_robopies/proof_of_concept/hand_tracking_module.py
@@ -0,0 +1,69 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class HandDetector:
+    def __init__(self, mode=False, max_hands=2, detection_con=0.5, track_con=0.5):
+        self.mode = mode
+        self.max_hands = max_hands
+        self.detection_con = detection_con
+        self.track_con = track_con
+
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(self.mode, self.max_hands, self.detection_con, self.track_con)
+        self.mp_draw = mp.solutions.drawing_utils
+
+    def find_hands(self, img, draw=True):
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(img_rgb)
+
+        if self.results.multi_hand_landmarks:
+            for hand_landmarks in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mp_draw.draw_landmarks(img, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+        return img
+
+    def find_position(self, img, hand_num=0, draw=True):
+        landmark_positions = []
+        if self.results.multi_hand_landmarks:
+            my_hand = self.results.multi_hand_landmarks[hand_num]
+
+            for id, landmark in enumerate(my_hand.landmark):
+                height, width, channels = img.shape
+                cx, cy = int(landmark.x * width), int(landmark.y * height)
+                landmark_positions.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+        return landmark_positions
+
+
+def main():
+    prev_time = 0
+    curr_time = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector()
+
+    while True:
+        success, img = cap.read()
+        img = detector.find_hands(img)
+        landmark_positions = detector.find_position(img, draw=False)
+        if len(landmark_positions) != 0:
+            print(landmark_positions[4])
+
+        curr_time = time.time()
+        fps = 1 / (curr_time - prev_time)
+        prev_time = curr_time
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), thickness=3)
+
+        cv2.imshow('Frame', img)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
